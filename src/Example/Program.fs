32e712a8891e53e3242ac9d788a98e08c4478e87
Tobias Burger <tobias_burger@hotmail.com>
2017-12-20 21:15:33 +0100

    Example now uses Sqlite in memoryDb

--- src/Example/Program.fs
+++ src/Example/Program.fs
@@ -1,6 +1,6 @@
 ﻿open FSharp.Sql
-open System.Data.SqlClient
 open System.Data
+open Microsoft.Data.Sqlite
 
 [<CLIMutable>]
 type User =
@@ -16,34 +16,60 @@ let tryGetUser id =
         "SELECT id, name FROM users WHERE id = @id"
 
 let getUserCount () =
-    Command.count (Table ("dbo", "users"))
+    Command.count (Table "users")
 
 let connectionString =
-    """Data Source=(localdb)\MSSQLLocalDB;
-       Initial Catalog=FSharp.Sql;
-       Integrated Security=True;
-       Connect Timeout=30;
-       Encrypt=False;
-       TrustServerCertificate=True;
-       ApplicationIntent=ReadWrite;
-       MultiSubnetFailover=False"""
+    """DataSource=:memory:"""
 
 let connectionCreator () =
-    new SqlConnection(connectionString) :> IDbConnection
+    new SqliteConnection(connectionString) :> IDbConnection
+
+let createUsersTable () =
+    let fields =
+        [ { Name = "id"; Type = Int }
+          { Name = "name"; Type = Varchar(50) } ]
+    Command.create (Table "users") fields
+    |> Sql.map ignore
+
+let insertData data =
+    let param name (value: obj) = SqliteParameter(name, value)
+    [ for id, name in data ->
+        Command.executeNonQueryWith
+            [ param "id" (box id)
+              param "name" (box name) ]
+            """INSERT INTO users
+                (id, name)
+               VALUES
+                (@id, @name)""" ]
+    // |> SqlExtras.Parallel
+    |> SqlExtras.sequence
+    |> Sql.map ignore
+
+let dropUsersTable () =
+    Command.drop (Table "users")
+    |> Sql.map ignore
 
 /// Very contrived example, but the idea is to have a composable
 /// workflow of SQL actions, that can be combined together
-let program = sql {
+let program data = sql {
+    do! createUsersTable ()
+    do! insertData data
     let! count = getUserCount ()
     let! users = getUsers ()
     let! user = tryGetUser 1
     //let! user = tryGetUser -1
+    do! dropUsersTable ()
     return (count, users, user)
 }
 
+let data =
+    [ 1, "Tobias"
+      2, "Lorenz"
+      3, "Stefanie" ]
+
 [<EntryPoint>]
 let main _ =
-    program
+    program data
     |> Sql.execute connectionCreator
     |> Async.RunSynchronously
     |> function
